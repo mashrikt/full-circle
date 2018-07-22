9d81233fbbb77ade5b4ea599c0ad9ffc541b2f31
Tahzib Mashrik <mashrikt@gmail.com>
2018-07-22 17:29:00 +0600

    remove celery command

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: bash -c "./bin/run-prod.sh"
-worker: celery -A fullcircle worker -l info
 
--- fullcircle/settings.py
+++ fullcircle/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY',
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS',
-                       default='localhost', cast=Csv())
+                       default='*', cast=Csv())
 
 
 # Application definition
